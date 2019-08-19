5767bc3aa1aa9d27423ebe23b49ab41de3897ca9
jgomezm-tecsup <jgomezm@tecsup.edu.pe>
2019-08-19 12:52:13 -0500

    Session 01

---
+++ session01/examples_pandas.py
@@ -0,0 +1,18 @@
+'''
+Programa : Ciencia de Datos con Pthon
+Modulo 02 : Estadística y Visualización de Datos con Python
+Sesion 01 : Importación de Datasets
+Fecha : 18/08/2019
+Version : 1
+Author : Jaime Gomez
+'''
+
+import pandas as pd
+
+
+paises = {"ARG":"Argentina","PER":"Perú","ECU":"Ecuador"}
+
+for key, value in paises.items():
+    print(key,"--->",value)
+
+
