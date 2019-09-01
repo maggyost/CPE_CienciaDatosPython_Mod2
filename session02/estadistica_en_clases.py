685126fea5fd8886883daec9bed084d08fa31d45
Jaime  Gomez Marin <jgomezm@tecsup.edu.pe>
2019-09-01 10:53:53 -0500

    Session 02

---
+++ session02/estadistica_en_clases.py
@@ -0,0 +1,47 @@
+'''
+
+'''
+
+import pandas as pd
+import numpy as np
+
+#df = pd.read_csv("medallero_Panamericanos_Lima2019.csv", header= 0)
+df = pd.read_csv("medallero_Panamericanos_Lima2019.csv")
+
+print(df.head())
+
+def calc_suma():
+    ''' Obtiene la suma de una columna de un dataframe'''
+    print(df['Oro'].sum())
+    print(df.Oro.sum())
+    print(np.sum(df['Oro']))
+    print(np.sum(df.Oro))
+
+def calc_nro_elementos():
+    print(len(df['Oro']))
+    print(len(df.Oro))
+    print(df['Oro'].count())
+    print(df.Oro.count())
+    print(np.size(df['Oro']))
+    print(np.size(df.Oro))
+
+def calc_media():
+    print( df.Oro.sum()/df.Oro.count())
+    print( df.Oro.mean())
+    print(np.mean(df.Oro))
+
+def obtener_media(redondeo = 2):
+    media = df.Oro.mean()
+    media =  round(media,redondeo)
+    return media
+
+if __name__ == '__main__':
+    #calc_suma()
+    #calc_nro_elementos()
+    #calc_media()
+    print(obtener_media())
+    print(obtener_media(redondeo = 4))
+    print(df.Oro.mean())
+    print(df.mean())
+    print(round(df.mean(),2))
+    print(type(df.mean()))
