8005fe3892fb43ed5d1fe33ffadda67b69ad66ea
jgomezm-tecsup <jgomezm@tecsup.edu.pe>
2019-09-01 09:32:35 -0500

    Session 02

--- session02/probabilidades.py
+++ session02/probabilidades.py
@@ -1,118 +1,81 @@
 '''
 
 '''
-import pandas as pd
+
 import numpy as np
-import scipy.stats as ss
 
-# Media
-print("---------------------------------")
-NOMBRE_ARCHIVO = "medallero_Panamericanos_Lima2019.csv"
-df = pd.read_csv(NOMBRE_ARCHIVO)
-print(df.head())
-#print(df['Oro'])
+def simulating_coin():
+    np.random.seed(42)
+    random_numbers = np.random.random(size=4)
+    print(random_numbers)
+    heads = random_numbers < 0.5
+    print(heads)
+    print(np.sum(heads))
 
-# Media
-def cal_media():
-    print("---------------------------------")
-    mean = df['Oro'].mean()
-    sumatoria = np.sum(df['Oro'])
-    nro_item = np.size(df['Oro'])
-    print("sumatoria ,", sumatoria)
-    print("elementos ,", nro_item)
-    print("media manual = ", sumatoria/nro_item)
-    print("media = ", mean)
-
-# Outliers
-def cal_outliers():
-    print("---------------------------------")
-    print("mayor valor = ", df['Oro'].max())
-    print("menor valor = ", df['Oro'].min())
-
-# Mediana
-def cal_mediana():
-    print("---------------------------------")
-    mediana = df['Oro'].median()
-    nro_item = np.size(df['Oro'])
-    pos_mediana = round(nro_item/2)
-    print("pos_mediana = ", pos_mediana)
-    print("mediana manual = ", df['Oro'][pos_mediana-1])
-    print("mediana = ", mediana)
-
-# Mode
-def cal_mode():
-    print("---------------------------------")
-    print(df['Oro'].value_counts())
-    mode = df['Oro'].mode()
-    print("moda = " , mode)
-
-# Coeficiente de correlacion
-def cal_correlacion():
-    print("---------------------------------")
-    print(df.corr().head())
+# Se lanza 10000 veces
+def simulating_coins():
+    n_all_heads = 0  # Initialize number of 4-heads trials
+    for _ in range(10000):
+        heads = np.random.random(size=4) < 0.5
+        n_heads = np.sum(heads)
+        if n_heads == 4:
+            n_all_heads += 1
+    print(n_all_heads / 10000)
 
 
-# Percentiles
-def cal_percentiles():
-    print("---------------------------------")
-    tramos = [25,50,75]
-    percentiles = np.percentile(df['Oro'],tramos)
-    print("percentiles = ", percentiles )
+# La distribucion Uniforme
 
-# Percentiles
-def cal_percentiles_2():
-    print("---------------------------------")
-    tramos = [25,50,75]
-    percentiles = df['Oro'].quantile(0.50)
-    print("percentiles = ", percentiles )
+# funcion de probabilidad uniforme discreta
+#  P(X=x) = 1/n
 
-# Grafico Percentiles
-def graph_percentiles():
+#The bus arrives at station one time in 20 minutes.
+#Some passenger came to the station at random time,
+#what the probability that passenger gets on the bus in 5
+#minutes interval? This time I will show this without code.
+
+
+def ecdf(data):
+    """ Compute ECDF """
+    x = np.sort(data)
+    n = x.size
+    y = np.arange(1, n+1) / n
+    return(x,y)
+
+
+# La distribucion Binomial
+def distribucion_binomial():
+    a = np.random.binomial(4, 0.5)
+    print(a)
+    a = np.random.binomial(4, 0.5, size=10)
+    print(a)
+    samples = np.random.binomial(60, 0.1, size=10000)
+    print(samples)
+
     import matplotlib.pyplot as plt
     import seaborn as sns
-    sns.boxplot(y="Oro", data= df)
+
+    sns.set()
+    x, y = ecdf(samples)
+    plt.plot(x, y, marker='.', linestyle='none')
+    plt.margins(0.02)
+    plt.xlabel('number of successes')
+    plt.ylabel('CDF')
     plt.show()
 
 
-# Grafico Percentiles
-def graph_percentiles_2():
-    import matplotlib.pyplot as plt
-    red_square = dict(markerfacecolor='r', marker='s')
-    fig1, ax1 = plt.subplots()
-    ax1.set_title('Mi primer Boxplot')
-    ax1.boxplot(df['Oro'], flierprops=red_square)
+# La distribucion Poisson
+def distribucion_poisson():
+    samples = np.random.poisson(6, size=10000)
+    x, y = ecdf(samples)
 
-# Grafico Paises x Medalla de Oro
-def graph_paises_med_oro():
     import matplotlib.pyplot as plt
-    import seaborn as sns
-    _ = sns.boxplot(x="Oro", y ="Pais", data= df)
+
+    plt.plot(x, y, marker='.', linestyle='none')
+    plt.margins(0.02)
+    plt.xlabel('number of successes')
+    plt.ylabel('CDF')
     plt.show()
-
-# Varianza
-def cal_varianza():
-    diff = df[['Oro']] - df['Oro'].mean()
-    print(type(diff))
-    diff2 = np.power(diff,2)
-    print(type(diff2))
-    nro_item = np.size(df['Oro'])
-    var_manual = (diff2.sum()/nro_item)['Oro']
-#    print(type(var_manual))
-    print("varianza manual = ", var_manual)
-
-    var = df['Oro'].var(ddof=0)  # corrección de Bessel
-    print("varianza = ", var)
-    print("varianza = ", np.var(df['Oro']))
-
-# Desviacion Estandar
-def cal_desviacion_estandar():
-    std_manual = np.sqrt(df['Oro'].var(ddof=0)) # corrección de Bessel
-    print("desviacion estandar manual = ", std_manual)
-    std = df['Oro'].std(ddof=0) # corrección de Bessel
-    print("desviacion estandar = ", std)
 
 
 if __name__ == '__main__':
-    #print(df.Oro.describe())
-    #cal_correlacion()
-    graph_percentiles()
+    distribucion_poisson()
